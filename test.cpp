5ad2642e2ebaed0d0f34161725124509982db894
Glenn Fiedler <glenn@networknext.com>
2023-07-07 12:49:56 -0400

    add hacked up test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*
+    Snapshot SDK Copyright © 2023 Network Next, Inc. This source code is licensed under GPL version 3 or any later version.
+    Commercial licenses under different terms are available. Contact licensing@mas-bandwidth.com for details.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    printf( "\nhello world\n\n" );
+    fflush( stdout );
+    return 0;
+}
