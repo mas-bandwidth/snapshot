88acb75d0d11e787ac9e0867b03546fb0eeeba56
Glenn Fiedler <glenn@networknext.com>
2023-07-08 10:36:26 -0400

    added basic read and write

---
+++ source/snapshot_read_write.h
@@ -0,0 +1,202 @@
+/*
+    Snapshot Copyright © 2023 Mas Bandwidth LLC. This source code is licensed under GPL version 3 or any later version.
+    Commercial licensing under different terms is available. Please email licensing@mas-bandwidth.com for details.
+*/
+
+#ifndef SNAPSHOT_READ_WRITE_H
+#define SNAPSHOT_READ_WRITE_H
+
+#include "snapshot_address.h"
+
+// ----------------------------------------------------------------------
+
+void snapshot_write_uint8( uint8_t ** p, uint8_t value )
+{
+    **p = value;
+    ++(*p);
+}
+
+void snapshot_write_uint16( uint8_t ** p, uint16_t value )
+{
+    (*p)[0] = value & 0xFF;
+    (*p)[1] = value >> 8;
+    *p += 2;
+}
+
+void snapshot_write_uint32( uint8_t ** p, uint32_t value )
+{
+    (*p)[0] = value & 0xFF;
+    (*p)[1] = ( value >> 8  ) & 0xFF;
+    (*p)[2] = ( value >> 16 ) & 0xFF;
+    (*p)[3] = value >> 24;
+    *p += 4;
+}
+
+void snapshot_write_uint64( uint8_t ** p, uint64_t value )
+{
+    (*p)[0] = value & 0xFF;
+    (*p)[1] = ( value >> 8  ) & 0xFF;
+    (*p)[2] = ( value >> 16 ) & 0xFF;
+    (*p)[3] = ( value >> 24 ) & 0xFF;
+    (*p)[4] = ( value >> 32 ) & 0xFF;
+    (*p)[5] = ( value >> 40 ) & 0xFF;
+    (*p)[6] = ( value >> 48 ) & 0xFF;
+    (*p)[7] = value >> 56;
+    *p += 8;
+}
+
+void snapshot_write_float32( uint8_t ** p, float value )
+{
+    uint32_t value_int = 0;
+    char * p_value = (char*)(&value);
+    char * p_value_int = (char*)(&value_int);
+    memcpy(p_value_int, p_value, sizeof(uint32_t));
+    snapshot_write_uint32( p, value_int);
+}
+
+void snapshot_write_float64( uint8_t ** p, double value )
+{
+    uint64_t value_int = 0;
+    char * p_value = (char *)(&value);
+    char * p_value_int = (char *)(&value_int);
+    memcpy(p_value_int, p_value, sizeof(uint64_t));
+    snapshot_write_uint64( p, value_int);
+}
+
+void snapshot_write_bytes( uint8_t ** p, const uint8_t * byte_array, int num_bytes )
+{
+    for ( int i = 0; i < num_bytes; ++i )
+    {
+        snapshot_write_uint8( p, byte_array[i] );
+    }
+}
+
+void snapshot_write_address( uint8_t ** buffer, const snapshot_address_t * address )
+{
+    snapshot_assert( buffer );
+    snapshot_assert( *buffer );
+    snapshot_assert( address );
+
+    if ( address->type == SNAPSHOT_ADDRESS_IPV4 )
+    {
+        snapshot_write_uint8( buffer, SNAPSHOT_ADDRESS_IPV4 );
+        for ( int i = 0; i < 4; ++i )
+        {
+            snapshot_write_uint8( buffer, address->data.ipv4[i] );
+        }
+        snapshot_write_uint16( buffer, address->port );
+    }
+    else if ( address->type == SNAPSHOT_ADDRESS_IPV6 )
+    {
+        snapshot_write_uint8( buffer, SNAPSHOT_ADDRESS_IPV6 );
+        for ( int i = 0; i < 8; ++i )
+        {
+            snapshot_write_uint16( buffer, address->data.ipv6[i] );
+        }
+        snapshot_write_uint16( buffer, address->port );
+    }
+    else
+    {
+        snapshot_write_uint8( buffer, SNAPSHOT_ADDRESS_NONE );    
+    }
+}
+
+// ----------------------------------------------------------------------
+
+uint8_t snapshot_read_uint8( const uint8_t ** p )
+{
+    uint8_t value = **p;
+    ++(*p);
+    return value;
+}
+
+uint16_t snapshot_read_uint16( const uint8_t ** p )
+{
+    uint16_t value;
+    value = (*p)[0];
+    value |= ( ( (uint16_t)( (*p)[1] ) ) << 8 );
+    *p += 2;
+    return value;
+}
+
+uint32_t snapshot_read_uint32( const uint8_t ** p )
+{
+    uint32_t value;
+    value  = (*p)[0];
+    value |= ( ( (uint32_t)( (*p)[1] ) ) << 8 );
+    value |= ( ( (uint32_t)( (*p)[2] ) ) << 16 );
+    value |= ( ( (uint32_t)( (*p)[3] ) ) << 24 );
+    *p += 4;
+    return value;
+}
+
+uint64_t snapshot_read_uint64( const uint8_t ** p )
+{
+    uint64_t value;
+    value  = (*p)[0];
+    value |= ( ( (uint64_t)( (*p)[1] ) ) << 8  );
+    value |= ( ( (uint64_t)( (*p)[2] ) ) << 16 );
+    value |= ( ( (uint64_t)( (*p)[3] ) ) << 24 );
+    value |= ( ( (uint64_t)( (*p)[4] ) ) << 32 );
+    value |= ( ( (uint64_t)( (*p)[5] ) ) << 40 );
+    value |= ( ( (uint64_t)( (*p)[6] ) ) << 48 );
+    value |= ( ( (uint64_t)( (*p)[7] ) ) << 56 );
+    *p += 8;
+    return value;
+}
+
+float snapshot_read_float32( const uint8_t ** p )
+{
+    uint32_t value_int = snapshot_read_uint32( p );
+    float value_float = 0.0f;
+    uint8_t * pointer_int = (uint8_t *)( &value_int );
+    uint8_t * pointer_float = (uint8_t *)( &value_float );
+    memcpy( pointer_float, pointer_int, sizeof( value_int ) );
+    return value_float;
+}
+
+double snapshot_read_float64( const uint8_t ** p )
+{
+    uint64_t value_int = snapshot_read_uint64( p );
+    double value_float = 0.0;
+    uint8_t * pointer_int = (uint8_t *)( &value_int );
+    uint8_t * pointer_float = (uint8_t *)( &value_float );
+    memcpy( pointer_float, pointer_int, sizeof( value_int ) );
+    return value_float;
+}
+
+void snapshot_read_bytes( const uint8_t ** p, uint8_t * byte_array, int num_bytes )
+{
+    for ( int i = 0; i < num_bytes; ++i )
+    {
+        byte_array[i] = snapshot_read_uint8( p );
+    }
+}
+
+void snapshot_read_address( const uint8_t ** buffer, snapshot_address_t * address )
+{
+    memset( address, 0, sizeof(snapshot_address_t) );
+
+    address->type = snapshot_read_uint8( buffer );
+
+    if ( address->type == SNAPSHOT_ADDRESS_IPV4 )
+    {
+        for ( int j = 0; j < 4; ++j )
+        {
+            address->data.ipv4[j] = snapshot_read_uint8( buffer );
+        }
+        address->port = snapshot_read_uint16( buffer );
+    }
+    else if ( address->type == SNAPSHOT_ADDRESS_IPV6 )
+    {
+        for ( int j = 0; j < 8; ++j )
+        {
+            address->data.ipv6[j] = snapshot_read_uint16( buffer );
+        }
+        address->port = snapshot_read_uint16( buffer );
+    }
+}
+
+// ----------------------------------------------------------------------
+
+#endif // #ifndef SNAPSHOT_READ_WRITE_H
--- test.cpp
+++ test.cpp
@@ -8,6 +8,8 @@
 #include "snapshot.h"
 #include "snapshot_crypto.h"
 #include "snapshot_platform.h"
+#include "snapshot_address.h"
+#include "snapshot_read_write.h"
 
 static void snapshot_check_handler( const char * condition,
                                     const char * function,
@@ -66,6 +68,223 @@ void test_endian()
 #endif // #if SNAPSHOT_LITTLE_ENDIAN
 }
 
+void test_address()
+{
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "" ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "[" ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "[]" ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "[]:" ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, ":" ) == SNAPSHOT_ERROR );
+#if !defined(WINVER) || WINVER > 0x502 // windows xp sucks
+        snapshot_check( snapshot_address_parse( &address, "1" ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "12" ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "123" ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "1234" ) == SNAPSHOT_ERROR );
+#endif
+        snapshot_check( snapshot_address_parse( &address, "1234.0.12313.0000" ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "1234.0.12313.0000.0.0.0.0.0" ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "1312313:123131:1312313:123131:1312313:123131:1312313:123131:1312313:123131:1312313:123131" ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "." ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, ".." ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "..." ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "...." ) == SNAPSHOT_ERROR );
+        snapshot_check( snapshot_address_parse( &address, "....." ) == SNAPSHOT_ERROR );
+    }
+
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "107.77.207.77" ) == SNAPSHOT_OK );
+        snapshot_check( address.type == SNAPSHOT_ADDRESS_IPV4 );
+        snapshot_check( address.port == 0 );
+        snapshot_check( address.data.ipv4[0] == 107 );
+        snapshot_check( address.data.ipv4[1] == 77 );
+        snapshot_check( address.data.ipv4[2] == 207 );
+        snapshot_check( address.data.ipv4[3] == 77 );
+    }
+
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "127.0.0.1" ) == SNAPSHOT_OK );
+        snapshot_check( address.type == SNAPSHOT_ADDRESS_IPV4 );
+        snapshot_check( address.port == 0 );
+        snapshot_check( address.data.ipv4[0] == 127 );
+        snapshot_check( address.data.ipv4[1] == 0 );
+        snapshot_check( address.data.ipv4[2] == 0 );
+        snapshot_check( address.data.ipv4[3] == 1 );
+    }
+
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "107.77.207.77:40000" ) == SNAPSHOT_OK );
+        snapshot_check( address.type == SNAPSHOT_ADDRESS_IPV4 );
+        snapshot_check( address.port == 40000 );
+        snapshot_check( address.data.ipv4[0] == 107 );
+        snapshot_check( address.data.ipv4[1] == 77 );
+        snapshot_check( address.data.ipv4[2] == 207 );
+        snapshot_check( address.data.ipv4[3] == 77 );
+    }
+
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "127.0.0.1:40000" ) == SNAPSHOT_OK );
+        snapshot_check( address.type == SNAPSHOT_ADDRESS_IPV4 );
+        snapshot_check( address.port == 40000 );
+        snapshot_check( address.data.ipv4[0] == 127 );
+        snapshot_check( address.data.ipv4[1] == 0 );
+        snapshot_check( address.data.ipv4[2] == 0 );
+        snapshot_check( address.data.ipv4[3] == 1 );
+    }
+
+#if SNAPSHOT_PLATFORM_HAS_IPV6
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "fe80::202:b3ff:fe1e:8329" ) == SNAPSHOT_OK );
+        snapshot_check( address.type == SNAPSHOT_ADDRESS_IPV6 );
+        snapshot_check( address.port == 0 );
+        snapshot_check( address.data.ipv6[0] == 0xfe80 );
+        snapshot_check( address.data.ipv6[1] == 0x0000 );
+        snapshot_check( address.data.ipv6[2] == 0x0000 );
+        snapshot_check( address.data.ipv6[3] == 0x0000 );
+        snapshot_check( address.data.ipv6[4] == 0x0202 );
+        snapshot_check( address.data.ipv6[5] == 0xb3ff );
+        snapshot_check( address.data.ipv6[6] == 0xfe1e );
+        snapshot_check( address.data.ipv6[7] == 0x8329 );
+    }
+
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "::" ) == SNAPSHOT_OK );
+        snapshot_check( address.type == SNAPSHOT_ADDRESS_IPV6 );
+        snapshot_check( address.port == 0 );
+        snapshot_check( address.data.ipv6[0] == 0x0000 );
+        snapshot_check( address.data.ipv6[1] == 0x0000 );
+        snapshot_check( address.data.ipv6[2] == 0x0000 );
+        snapshot_check( address.data.ipv6[3] == 0x0000 );
+        snapshot_check( address.data.ipv6[4] == 0x0000 );
+        snapshot_check( address.data.ipv6[5] == 0x0000 );
+        snapshot_check( address.data.ipv6[6] == 0x0000 );
+        snapshot_check( address.data.ipv6[7] == 0x0000 );
+    }
+
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "::1" ) == SNAPSHOT_OK );
+        snapshot_check( address.type == SNAPSHOT_ADDRESS_IPV6 );
+        snapshot_check( address.port == 0 );
+        snapshot_check( address.data.ipv6[0] == 0x0000 );
+        snapshot_check( address.data.ipv6[1] == 0x0000 );
+        snapshot_check( address.data.ipv6[2] == 0x0000 );
+        snapshot_check( address.data.ipv6[3] == 0x0000 );
+        snapshot_check( address.data.ipv6[4] == 0x0000 );
+        snapshot_check( address.data.ipv6[5] == 0x0000 );
+        snapshot_check( address.data.ipv6[6] == 0x0000 );
+        snapshot_check( address.data.ipv6[7] == 0x0001 );
+    }
+
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "[fe80::202:b3ff:fe1e:8329]:40000" ) == SNAPSHOT_OK );
+        snapshot_check( address.type == SNAPSHOT_ADDRESS_IPV6 );
+        snapshot_check( address.port == 40000 );
+        snapshot_check( address.data.ipv6[0] == 0xfe80 );
+        snapshot_check( address.data.ipv6[1] == 0x0000 );
+        snapshot_check( address.data.ipv6[2] == 0x0000 );
+        snapshot_check( address.data.ipv6[3] == 0x0000 );
+        snapshot_check( address.data.ipv6[4] == 0x0202 );
+        snapshot_check( address.data.ipv6[5] == 0xb3ff );
+        snapshot_check( address.data.ipv6[6] == 0xfe1e );
+        snapshot_check( address.data.ipv6[7] == 0x8329 );
+    }
+
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "[::]:40000" ) == SNAPSHOT_OK );
+        snapshot_check( address.type == SNAPSHOT_ADDRESS_IPV6 );
+        snapshot_check( address.port == 40000 );
+        snapshot_check( address.data.ipv6[0] == 0x0000 );
+        snapshot_check( address.data.ipv6[1] == 0x0000 );
+        snapshot_check( address.data.ipv6[2] == 0x0000 );
+        snapshot_check( address.data.ipv6[3] == 0x0000 );
+        snapshot_check( address.data.ipv6[4] == 0x0000 );
+        snapshot_check( address.data.ipv6[5] == 0x0000 );
+        snapshot_check( address.data.ipv6[6] == 0x0000 );
+        snapshot_check( address.data.ipv6[7] == 0x0000 );
+    }
+
+    {
+        struct snapshot_address_t address;
+        snapshot_check( snapshot_address_parse( &address, "[::1]:40000" ) == SNAPSHOT_OK );
+        snapshot_check( address.type == SNAPSHOT_ADDRESS_IPV6 );
+        snapshot_check( address.port == 40000 );
+        snapshot_check( address.data.ipv6[0] == 0x0000 );
+        snapshot_check( address.data.ipv6[1] == 0x0000 );
+        snapshot_check( address.data.ipv6[2] == 0x0000 );
+        snapshot_check( address.data.ipv6[3] == 0x0000 );
+        snapshot_check( address.data.ipv6[4] == 0x0000 );
+        snapshot_check( address.data.ipv6[5] == 0x0000 );
+        snapshot_check( address.data.ipv6[6] == 0x0000 );
+        snapshot_check( address.data.ipv6[7] == 0x0001 );
+    }
+#endif // #if SNAPSHOT_PLATFORM_HAS_IPV6
+}
+
+void test_read_and_write()
+{
+    uint8_t buffer[1024];
+
+    struct snapshot_address_t address_a, address_b, address_c;
+
+    memset( &address_a, 0, sizeof(address_a) );
+    memset( &address_b, 0, sizeof(address_b) );
+    memset( &address_c, 0, sizeof(address_c) );
+
+    snapshot_address_parse( &address_b, "127.0.0.1:50000" );
+
+    snapshot_address_parse( &address_c, "[::1]:50000" );
+
+    uint8_t * p = buffer;
+    snapshot_write_uint8( &p, 105 );
+    snapshot_write_uint16( &p, 10512 );
+    snapshot_write_uint32( &p, 105120000 );
+    snapshot_write_uint64( &p, 105120000000000000LL );
+    snapshot_write_float32( &p, 100.0f );
+    snapshot_write_float64( &p, 100000000000000.0 );
+    snapshot_write_bytes( &p, (uint8_t*)"hello", 6 );
+    snapshot_write_address( &p, &address_a );
+    snapshot_write_address( &p, &address_b );
+    snapshot_write_address( &p, &address_c );
+
+    const uint8_t * q = buffer;
+
+    uint8_t a = snapshot_read_uint8( &q );
+    uint16_t b = snapshot_read_uint16( &q );
+    uint32_t c = snapshot_read_uint32( &q );
+    uint64_t d = snapshot_read_uint64( &q );
+    float e = snapshot_read_float32( &q );
+    double f = snapshot_read_float64( &q );
+    uint8_t g[6];
+    snapshot_read_bytes( &q, g, 6 );
+
+    struct snapshot_address_t read_address_a, read_address_b, read_address_c;
+
+    snapshot_read_address( &q, &read_address_a );
+    snapshot_read_address( &q, &read_address_b );
+    snapshot_read_address( &q, &read_address_c );
+
+    snapshot_check( a == 105 );
+    snapshot_check( b == 10512 );
+    snapshot_check( c == 105120000 );
+    snapshot_check( d == 105120000000000000LL );
+    snapshot_check( e == 100.0f );
+    snapshot_check( f == 100000000000000.0 );
+    snapshot_check( memcmp( g, "hello", 6 ) == 0 );
+    snapshot_check( snapshot_address_equal( &address_a, &read_address_a ) );
+    snapshot_check( snapshot_address_equal( &address_b, &read_address_b ) );
+    snapshot_check( snapshot_address_equal( &address_c, &read_address_c ) );
+}
+
 #define RUN_TEST( test_function )                                           \
     do                                                                      \
     {                                                                       \
@@ -81,10 +300,10 @@ void test()
     {
         RUN_TEST( test_time );
         RUN_TEST( test_endian );
-
-        /*
         RUN_TEST( test_address );
         RUN_TEST( test_read_and_write );
+
+        /*
         RUN_TEST( test_bitpacker );
         RUN_TEST( test_bits_required );
         RUN_TEST( test_stream );
