eb663730528dba4e1684290a603259df9fa5170b
Glenn Fiedler <glenn@networknext.com>
2023-07-09 17:43:02 -0400

    got replay protection in

--- test.cpp
+++ test.cpp
@@ -21,6 +21,7 @@
 #include "snapshot_challenge_token.h"
 #include "snapshot_packets.h"
 #include "snapshot_encryption_manager.h"
+#include "snapshot_replay_protection.h"
 
 static void snapshot_check_handler( const char * condition,
                                     const char * function,
@@ -1605,6 +1606,52 @@ void test_encryption_manager()
     snapshot_check( snapshot_encryption_manager_find_encryption_mapping( &encryption_manager, &encryption_mapping[0].address, time ) == encryption_index );
 }
 
+void test_replay_protection()
+{
+    struct snapshot_replay_protection_t replay_protection;
+
+    int i;
+    for ( i = 0; i < 2; ++i )
+    {
+        snapshot_replay_protection_reset( &replay_protection );
+
+        snapshot_check( replay_protection.most_recent_sequence == 0 );
+
+        // the first time we receive packets, they should not be already received
+
+        #define MAX_SEQUENCE ( SNAPSHOT_REPLAY_PROTECTION_BUFFER_SIZE * 4 )
+
+        uint64_t sequence;
+        for ( sequence = 0; sequence < MAX_SEQUENCE; ++sequence )
+        {
+            snapshot_check( snapshot_replay_protection_already_received( &replay_protection, sequence ) == 0 );
+            snapshot_replay_protection_advance_sequence( &replay_protection, sequence );
+        }
+
+        // old packets outside buffer should be considered already received
+
+        snapshot_check( snapshot_replay_protection_already_received( &replay_protection, 0 ) == 1 );
+
+        // packets received a second time should be flagged already received
+
+        for ( sequence = MAX_SEQUENCE - 10; sequence < MAX_SEQUENCE; ++sequence )
+        {
+            snapshot_check( snapshot_replay_protection_already_received( &replay_protection, sequence ) == 1 );
+        }
+
+        // jumping ahead to a much higher sequence should be considered not already received
+
+        snapshot_check( snapshot_replay_protection_already_received( &replay_protection, MAX_SEQUENCE + SNAPSHOT_REPLAY_PROTECTION_BUFFER_SIZE ) == 0 );
+
+        // old packets should be considered already received
+
+        for ( sequence = 0; sequence < MAX_SEQUENCE; ++sequence )
+        {
+            snapshot_check( snapshot_replay_protection_already_received( &replay_protection, sequence ) == 1 );
+        }
+    }
+}
+
 #define RUN_TEST( test_function )                                           \
     do                                                                      \
     {                                                                       \
@@ -1646,9 +1693,9 @@ void test()
         RUN_TEST( test_connection_payload_packet );
         RUN_TEST( test_connection_disconnect_packet );        
         RUN_TEST( test_encryption_manager );
+        RUN_TEST( test_replay_protection );
 
         /*
-        RUN_TEST( test_replay_protection );
         RUN_TEST( test_client_create );
         RUN_TEST( test_server_create );
         RUN_TEST( test_client_server_connect );
