6e9f243981cc7c428baad1a386ed9352f677d036
Glenn Fiedler <glenn@networknext.com>
2023-07-08 20:56:45 -0400

    update license to include netcode

--- source/snapshot_network_simulator.h
+++ source/snapshot_network_simulator.h
@@ -11,19 +11,19 @@
 // todo: clean up
 
 /*
-#define NETCODE_NETWORK_SIMULATOR_NUM_PACKET_ENTRIES ( NETCODE_MAX_CLIENTS * 256 )
-#define NETCODE_NETWORK_SIMULATOR_NUM_PENDING_RECEIVE_PACKETS ( NETCODE_MAX_CLIENTS * 64 )
+#define SNAPSHOT_NETWORK_SIMULATOR_NUM_PACKET_ENTRIES ( SNAPSHOT_MAX_CLIENTS * 1024 )
+#define SNAPSHOT_NETWORK_SIMULATOR_NUM_PENDING_RECEIVE_PACKETS ( SNAPSHOT_MAX_CLIENTS * 256 )
 
-struct netcode_network_simulator_packet_entry_t
+struct snapshot_network_simulator_packet_entry_t
 {
-    struct netcode_address_t from;
-    struct netcode_address_t to;
+    struct snapshot_address_t from;
+    struct snapshot_address_t to;
     double delivery_time;
     uint8_t * packet_data;
     int packet_bytes;
 };
 
-struct netcode_network_simulator_t
+struct snapshot_network_simulator_t
 {
     void * allocator_context;
     void * (*allocate_function)(void*,uint64_t);
@@ -35,30 +35,30 @@ struct netcode_network_simulator_t
     double time;
     int current_index;
     int num_pending_receive_packets;
-    struct netcode_network_simulator_packet_entry_t packet_entries[NETCODE_NETWORK_SIMULATOR_NUM_PACKET_ENTRIES];
-    struct netcode_network_simulator_packet_entry_t pending_receive_packets[NETCODE_NETWORK_SIMULATOR_NUM_PENDING_RECEIVE_PACKETS];
+    struct snapshot_network_simulator_packet_entry_t packet_entries[SNAPSHOT_NETWORK_SIMULATOR_NUM_PACKET_ENTRIES];
+    struct snapshot_network_simulator_packet_entry_t pending_receive_packets[SNAPSHOT_NETWORK_SIMULATOR_NUM_PENDING_RECEIVE_PACKETS];
 };
 
-struct netcode_network_simulator_t * netcode_network_simulator_create( void * allocator_context, 
-                                                                       void * (*allocate_function)(void*,uint64_t), 
-                                                                       void (*free_function)(void*,void*) )
+struct snapshot_network_simulator_t * snapshot_network_simulator_create( void * allocator_context, 
+                                                                         void * (*allocate_function)(void*,uint64_t), 
+                                                                         void (*free_function)(void*,void*) )
 {
     if ( allocate_function == NULL )
     {
-        allocate_function = netcode_default_allocate_function;
+        allocate_function = snapshot_default_allocate_function;
     }
 
     if ( free_function == NULL )
     {
-        free_function = netcode_default_free_function;
+        free_function = snapshot_default_free_function;
     }
 
-    struct netcode_network_simulator_t * network_simulator = (struct netcode_network_simulator_t*) 
-        allocate_function( allocator_context, sizeof( struct netcode_network_simulator_t ) );
+    struct snapshot_network_simulator_t * network_simulator = (struct snapshot_network_simulator_t*) 
+        allocate_function( allocator_context, sizeof( struct snapshot_network_simulator_t ) );
 
-    netcode_assert( network_simulator );
+    snapshot_assert( network_simulator );
 
-    memset( network_simulator, 0, sizeof( struct netcode_network_simulator_t ) );
+    memset( network_simulator, 0, sizeof( struct snapshot_network_simulator_t ) );
 
     network_simulator->allocator_context = allocator_context;
     network_simulator->allocate_function = allocate_function;
@@ -67,51 +67,51 @@ struct netcode_network_simulator_t * netcode_network_simulator_create( void * al
     return network_simulator;
 }
 
-void netcode_network_simulator_reset( struct netcode_network_simulator_t * network_simulator )
+void snapshot_network_simulator_reset( struct snapshot_network_simulator_t * network_simulator )
 {
-    netcode_assert( network_simulator );
+    snapshot_assert( network_simulator );
 
-    netcode_printf( NETCODE_LOG_LEVEL_DEBUG, "network simulator reset\n" );
+    snapshot_printf( SNAPSHOT_LOG_LEVEL_DEBUG, "network simulator reset\n" );
 
     int i;
-    for ( i = 0; i < NETCODE_NETWORK_SIMULATOR_NUM_PACKET_ENTRIES; ++i )
+    for ( i = 0; i < SNAPSHOT_NETWORK_SIMULATOR_NUM_PACKET_ENTRIES; ++i )
     {
         network_simulator->free_function( network_simulator->allocator_context, network_simulator->packet_entries[i].packet_data );
-        memset( &network_simulator->packet_entries[i], 0, sizeof( struct netcode_network_simulator_packet_entry_t ) );
+        memset( &network_simulator->packet_entries[i], 0, sizeof( struct snapshot_network_simulator_packet_entry_t ) );
     }
 
     for ( i = 0; i < network_simulator->num_pending_receive_packets; ++i )
     {
         network_simulator->free_function( network_simulator->allocator_context, network_simulator->pending_receive_packets[i].packet_data );
-        memset( &network_simulator->pending_receive_packets[i], 0, sizeof( struct netcode_network_simulator_packet_entry_t ) );
+        memset( &network_simulator->pending_receive_packets[i], 0, sizeof( struct snapshot_network_simulator_packet_entry_t ) );
     }
 
     network_simulator->current_index = 0;
     network_simulator->num_pending_receive_packets = 0;
 }
 
-void netcode_network_simulator_destroy( struct netcode_network_simulator_t * network_simulator )
+void snapshot_network_simulator_destroy( struct snapshot_network_simulator_t * network_simulator )
 {
-    netcode_assert( network_simulator );
-    netcode_network_simulator_reset( network_simulator );
+    snapshot_assert( network_simulator );
+    snapshot_network_simulator_reset( network_simulator );
     network_simulator->free_function( network_simulator->allocator_context, network_simulator );
 }
 
-float netcode_random_float( float a, float b )
+float snapshot_random_float( float a, float b )
 {
-    netcode_assert( a < b );
+    snapshot_assert( a < b );
     float random = ( (float) rand() ) / (float) RAND_MAX;
     float diff = b - a;
     float r = random * diff;
     return a + r;
 }
 
-void netcode_network_simulator_queue_packet( struct netcode_network_simulator_t * network_simulator, 
-                                             struct netcode_address_t * from, 
-                                             struct netcode_address_t * to, 
-                                             uint8_t * packet_data, 
-                                             int packet_bytes, 
-                                             float delay )
+void snapshot_network_simulator_queue_packet( struct snapshot_network_simulator_t * network_simulator, 
+                                              struct snapshot_address_t * from, 
+                                              struct snapshot_address_t * to, 
+                                              uint8_t * packet_data, 
+                                              int packet_bytes, 
+                                              float delay )
 {
     network_simulator->packet_entries[network_simulator->current_index].from = *from;
     network_simulator->packet_entries[network_simulator->current_index].to = *to;
@@ -121,25 +121,25 @@ void netcode_network_simulator_queue_packet( struct netcode_network_simulator_t 
     network_simulator->packet_entries[network_simulator->current_index].packet_bytes = packet_bytes;
     network_simulator->packet_entries[network_simulator->current_index].delivery_time = network_simulator->time + delay;
     network_simulator->current_index++;
-    network_simulator->current_index %= NETCODE_NETWORK_SIMULATOR_NUM_PACKET_ENTRIES;
+    network_simulator->current_index %= SNAPSHOT_NETWORK_SIMULATOR_NUM_PACKET_ENTRIES;
 }
 
-void netcode_network_simulator_send_packet( struct netcode_network_simulator_t * network_simulator, 
-                                            struct netcode_address_t * from, 
-                                            struct netcode_address_t * to, 
-                                            uint8_t * packet_data, 
-                                            int packet_bytes )
+void snapshot_network_simulator_send_packet( struct snapshot_network_simulator_t * network_simulator, 
+                                             struct snapshot_address_t * from, 
+                                             struct snapshot_address_t * to, 
+                                             uint8_t * packet_data, 
+                                             int packet_bytes )
 {
-    netcode_assert( network_simulator );
-    netcode_assert( from );
-    netcode_assert( from->type != 0 );
-    netcode_assert( to );
-    netcode_assert( to->type != 0 );
-    netcode_assert( packet_data );
-    netcode_assert( packet_bytes > 0 );
-    netcode_assert( packet_bytes <= NETCODE_MAX_PACKET_BYTES );
+    snapshot_assert( network_simulator );
+    snapshot_assert( from );
+    snapshot_assert( from->type != 0 );
+    snasphot_assert( to );
+    snapshot_assert( to->type != 0 );
+    snapshot_assert( packet_data );
+    snapshot_assert( packet_bytes > 0 );
+    snapshot_assert( packet_bytes <= SNAPSHOT_MAX_PACKET_BYTES );
 
-    if ( netcode_random_float( 0.0f, 100.0f ) <= network_simulator->packet_loss_percent )
+    if ( snapshot_random_float( 0.0f, 100.0f ) <= network_simulator->packet_loss_percent )
         return;
 
     if ( network_simulator->packet_entries[network_simulator->current_index].packet_data )
@@ -151,29 +151,29 @@ void netcode_network_simulator_send_packet( struct netcode_network_simulator_t *
     float delay = network_simulator->latency_milliseconds / 1000.0f;
 
     if ( network_simulator->jitter_milliseconds > 0.0 )
-        delay += netcode_random_float( -network_simulator->jitter_milliseconds, +network_simulator->jitter_milliseconds ) / 1000.0f;
+        delay += snapshot_random_float( -network_simulator->jitter_milliseconds, +network_simulator->jitter_milliseconds ) / 1000.0f;
 
-    netcode_network_simulator_queue_packet( network_simulator, from, to, packet_data, packet_bytes, delay );
+    snapshot_network_simulator_queue_packet( network_simulator, from, to, packet_data, packet_bytes, delay );
 
-    if ( netcode_random_float( 0.0f, 100.0f ) <= network_simulator->duplicate_packet_percent )
+    if ( snapshot_random_float( 0.0f, 100.0f ) <= network_simulator->duplicate_packet_percent )
     {
-        netcode_network_simulator_queue_packet( network_simulator, from, to, packet_data, packet_bytes, delay + netcode_random_float( 0, 1.0 ) );
+        snapshot_network_simulator_queue_packet( network_simulator, from, to, packet_data, packet_bytes, delay + snapshot_random_float( 0, 1.0 ) );
     }
 }
 
-int netcode_network_simulator_receive_packets( struct netcode_network_simulator_t * network_simulator, 
-                                               struct netcode_address_t * to, 
-                                               int max_packets, 
-                                               uint8_t ** packet_data, 
-                                               int * packet_bytes, 
-                                               struct netcode_address_t * from )
+int snapshot_network_simulator_receive_packets( struct snapshot_network_simulator_t * network_simulator, 
+                                                struct snapshot_address_t * to, 
+                                                int max_packets, 
+                                                uint8_t ** packet_data, 
+                                                int * packet_bytes, 
+                                                struct snapshot_address_t * from )
 {
-    netcode_assert( network_simulator );
-    netcode_assert( max_packets >= 0 );
-    netcode_assert( packet_data );
-    netcode_assert( packet_bytes );
-    netcode_assert( from );
-    netcode_assert( to );
+    snapshot_assert( network_simulator );
+    snapshot_assert( max_packets >= 0 );
+    snapshot_assert( packet_data );
+    snapshot_assert( packet_bytes );
+    snapshot_assert( from );
+    snapshot_assert( to );
 
     int num_packets = 0;
 
@@ -186,7 +186,7 @@ int netcode_network_simulator_receive_packets( struct netcode_network_simulator_
         if ( !network_simulator->pending_receive_packets[i].packet_data )
             continue;
 
-        if ( !netcode_address_equal( &network_simulator->pending_receive_packets[i].to, to ) )
+        if ( !snapshot_address_equal( &network_simulator->pending_receive_packets[i].to, to ) )
             continue;
 
         packet_data[num_packets] = network_simulator->pending_receive_packets[i].packet_data;
@@ -198,14 +198,14 @@ int netcode_network_simulator_receive_packets( struct netcode_network_simulator_
         num_packets++;
     }
 
-    netcode_assert( num_packets <= max_packets );
+    snapshot_assert( num_packets <= max_packets );
 
     return num_packets;
 }
 
-void netcode_network_simulator_update( struct netcode_network_simulator_t * network_simulator, double time )
+void snapshot_network_simulator_update( struct snapshot_network_simulator_t * network_simulator, double time )
 {   
-    netcode_assert( network_simulator );
+    snapshot_assert( network_simulator );
 
     network_simulator->time = time;
 
@@ -225,12 +225,12 @@ void netcode_network_simulator_update( struct netcode_network_simulator_t * netw
 
     // walk across packet entries and move any that are ready to be received into the pending receive buffer
 
-    for ( i = 0; i < NETCODE_NETWORK_SIMULATOR_NUM_PACKET_ENTRIES; ++i )
+    for ( i = 0; i < SNAPSHOT_NETWORK_SIMULATOR_NUM_PACKET_ENTRIES; ++i )
     {
         if ( !network_simulator->packet_entries[i].packet_data )
             continue;
 
-        if ( network_simulator->num_pending_receive_packets == NETCODE_NETWORK_SIMULATOR_NUM_PENDING_RECEIVE_PACKETS )
+        if ( network_simulator->num_pending_receive_packets == SNAPSHOT_NETWORK_SIMULATOR_NUM_PENDING_RECEIVE_PACKETS )
             break;
 
         if ( network_simulator->packet_entries[i].packet_data && network_simulator->packet_entries[i].delivery_time <= time )
--- source/snapshot_server.h
+++ source/snapshot_server.h
@@ -9,5 +9,73 @@
 #include "snapshot.h"
 
 // todo
+/*
+struct snapshot_server_config_t
+{
+    uint64_t protocol_id;
+    uint8_t private_key[SNAPSHOT_KEY_BYTES];
+    void * allocator_context;
+    void * (*allocate_function)(void*,uint64_t);
+    void (*free_function)(void*,void*);
+    struct snapshot_network_simulator_t * network_simulator;
+    void * callback_context;
+    void (*connect_disconnect_callback)(void*,int,int);
+    void (*send_loopback_packet_callback)(void*,int,const uint8_t*,int,uint64_t);
+    int override_send_and_receive;
+    void (*send_packet_override)(void*,struct snapshot_address_t*,const uint8_t*,int);
+    int (*receive_packet_override)(void*,struct snapshot_address_t*,uint8_t*,int);
+};
+
+void snapshot_default_server_config( struct snapshot_server_config_t * config );
+
+struct snapshot_server_t * snapshot_server_create( const char * server_address, const struct snapshot_server_config_t * config, double time );
+
+void snapshot_server_destroy( struct snapshot_server_t * server );
+
+void snapshot_server_start( struct snapshot_server_t * server, int max_clients );
+
+void snapshot_server_stop( struct snapshot_server_t * server );
+
+int snapshot_server_running( struct snapshot_server_t * server );
+
+int snapshot_server_max_clients( struct snapshot_server_t * server );
+
+void snapshot_server_update( struct snapshot_server_t * server, double time );
+
+int snapshot_server_client_connected( struct snapshot_server_t * server, int client_index );
+
+uint64_t snapshot_server_client_id( struct snapshot_server_t * server, int client_index );
+
+struct snapshot_address_t * snapshot_server_client_address( struct snapshot_server_t * server, int client_index );
+
+void snapshot_server_disconnect_client( struct snapshot_server_t * server, int client_index );
+
+void snapshot_server_disconnect_all_clients( struct snapshot_server_t * server );
+
+uint64_t snapshot_server_next_packet_sequence( struct snapshot_server_t * server, int client_index );
+
+void snapshot_server_send_packet( struct snapshot_server_t * server, int client_index, const * packet_data, int packet_bytes );
+
+uint8_t * snapshot_server_receive_packet( struct snapshot_server_t * server, int client_index, int * packet_bytes, uint64_t * packet_sequence );
+
+void snapshot_server_free_packet( struct snapshot_server_t * server, void * packet );
+
+int snapshot_server_num_connected_clients( struct snapshot_server_t * server );
+
+void * snapshot_server_client_user_data( struct snapshot_server_t * server, int client_index );
+
+void snapshot_server_process_packet( struct snapshot_server_t * server, struct snapshot_address_t * from, uint8_t * packet_data, int packet_bytes );
+
+void snapshot_server_connect_loopback_client( struct snapshot_server_t * server, int client_index, uint64_t client_id, const uint8_t * user_data );
+
+void snapshot_server_disconnect_loopback_client( struct snapshot_server_t * server, int client_index );
+
+int snapshot_server_client_loopback( struct snapshot_server_t * server, int client_index );
+
+void snapshot_server_process_loopback_packet( struct snapshot_server_t * server, int client_index, const uint8_t * packet_data, int packet_bytes, uint64_t packet_sequence );
+
+uint16_t snapshot_server_get_port( struct snapshot_server_t * server );
+*/
 
 #endif // #ifndef SNAPSHOT_SERVER_H
+
