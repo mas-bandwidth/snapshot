c608658df14b0c7e18599c17ee1d60976dacc85f
Glenn Fiedler <glenn@networknext.com>
2023-07-12 11:19:12 -0400

    one more test

--- test.cpp
+++ test.cpp
@@ -2858,138 +2858,118 @@ void test_server_side_disconnect()
     snapshot_client_destroy( client );
 }
 
-/*
 void test_client_reconnect()
 {
-    struct netcode_network_simulator_t * network_simulator = netcode_network_simulator_create( NULL, NULL, NULL );
-
-    network_simulator->latency_milliseconds = 250;
-    network_simulator->jitter_milliseconds = 250;
-    network_simulator->packet_loss_percent = 5;
-    network_simulator->duplicate_packet_percent = 10;
+    uint8_t private_key[SNAPSHOT_KEY_BYTES];
+    snapshot_crypto_random_bytes( private_key, SNAPSHOT_KEY_BYTES );
 
     // start a server and connect one client
 
     double time = 0.0;
     double delta_time = 1.0 / 10.0;
 
-    struct netcode_client_config_t client_config;
-    netcode_default_client_config( &client_config );
-    client_config.network_simulator = network_simulator;
+    struct snapshot_client_config_t client_config;
+    snapshot_default_client_config( &client_config );
 
-    struct netcode_client_t * client = netcode_client_create( "[::]:50000", &client_config, time );
+    struct snapshot_client_t * client = snapshot_client_create( "0.0.0.0:30000", &client_config, time );
 
-    check( client );
+    snapshot_check( client );
 
-    struct netcode_server_config_t server_config;
-    netcode_default_server_config( &server_config );
+    struct snapshot_server_config_t server_config;
+    snapshot_default_server_config( &server_config );
     server_config.protocol_id = TEST_PROTOCOL_ID;
-    server_config.network_simulator = network_simulator;
-    memcpy( &server_config.private_key, private_key, NETCODE_KEY_BYTES );
+    memcpy( &server_config.private_key, private_key, SNAPSHOT_KEY_BYTES );
 
-    struct netcode_server_t * server = netcode_server_create( "[::1]:40000", &server_config, time );
+    struct snapshot_server_t * server = snapshot_server_create( "127.0.0.1:40000", &server_config, time );
 
-    check( server );
+    snapshot_check( server );
 
-    netcode_server_start( server, 1 );
+    snapshot_server_start( server, 1 );
 
-    NETCODE_CONST char * server_address = "[::1]:40000";
+    const char * server_address = "127.0.0.1:40000";
 
-    uint8_t connect_token[NETCODE_CONNECT_TOKEN_BYTES];
+    uint8_t connect_token[SNAPSHOT_CONNECT_TOKEN_BYTES];
 
     uint64_t client_id = 0;
-    netcode_random_bytes( (uint8_t*) &client_id, 8 );
+    snapshot_crypto_random_bytes( (uint8_t*) &client_id, 8 );
 
-    uint8_t user_data[NETCODE_USER_DATA_BYTES];
-    netcode_random_bytes(user_data, NETCODE_USER_DATA_BYTES);
+    uint8_t user_data[SNAPSHOT_USER_DATA_BYTES];
+    snapshot_crypto_random_bytes(user_data, SNAPSHOT_USER_DATA_BYTES);
 
-    check( netcode_generate_connect_token( 1, &server_address, &server_address, TEST_CONNECT_TOKEN_EXPIRY, TEST_TIMEOUT_SECONDS, client_id, TEST_PROTOCOL_ID, private_key, user_data, connect_token ) );
+    snapshot_check( snapshot_generate_connect_token( 1, &server_address, &server_address, TEST_CONNECT_TOKEN_EXPIRY, TEST_TIMEOUT_SECONDS, client_id, TEST_PROTOCOL_ID, private_key, user_data, connect_token ) == SNAPSHOT_OK );
 
-    netcode_client_connect( client, connect_token );
+    snapshot_client_connect( client, connect_token );
 
     while ( 1 )
     {
-        netcode_network_simulator_update( network_simulator, time );
+        snapshot_client_update( client, time );
 
-        netcode_client_update( client, time );
+        snapshot_server_update( server, time );
 
-        netcode_server_update( server, time );
-
-        if ( netcode_client_state( client ) <= NETCODE_CLIENT_STATE_DISCONNECTED )
+        if ( snapshot_client_state( client ) <= SNAPSHOT_CLIENT_STATE_DISCONNECTED )
             break;
 
-        if ( netcode_client_state( client ) == NETCODE_CLIENT_STATE_CONNECTED )
+        if ( snapshot_client_state( client ) == SNAPSHOT_CLIENT_STATE_CONNECTED )
             break;
 
         time += delta_time;
     }
 
-    check( netcode_client_state( client ) == NETCODE_CLIENT_STATE_CONNECTED );
-    check( netcode_client_index( client ) == 0 );
-    check( netcode_server_client_connected( server, 0 ) == 1 );
-    check( netcode_server_num_connected_clients( server ) == 1 );
+    snapshot_check( snapshot_client_state( client ) == SNAPSHOT_CLIENT_STATE_CONNECTED );
+    snapshot_check( snapshot_client_index( client ) == 0 );
+    snapshot_check( snapshot_server_client_connected( server, 0 ) == 1 );
+    snapshot_check( snapshot_server_num_connected_clients( server ) == 1 );
 
     // disconnect client on the server-side and wait until client sees the disconnect
 
-    netcode_network_simulator_reset( network_simulator );
-
-    netcode_server_disconnect_client( server, 0 );
+    snapshot_server_disconnect_client( server, 0 );
 
     while ( 1 )
     {
-        netcode_network_simulator_update( network_simulator, time );
+        snapshot_client_update( client, time );
 
-        netcode_client_update( client, time );
+        snapshot_server_update( server, time );
 
-        netcode_server_update( server, time );
-
-        if ( netcode_client_state( client ) <= NETCODE_CLIENT_STATE_DISCONNECTED )
+        if ( snapshot_client_state( client ) <= SNAPSHOT_CLIENT_STATE_DISCONNECTED )
             break;
 
         time += delta_time;
     }
 
-    check( netcode_client_state( client ) == NETCODE_CLIENT_STATE_DISCONNECTED );
-    check( netcode_server_client_connected( server, 0 ) == 0 );
-    check( netcode_server_num_connected_clients( server ) == 0 );
+    snapshot_check( snapshot_client_state( client ) == SNAPSHOT_CLIENT_STATE_DISCONNECTED );
+    snapshot_check( snapshot_server_client_connected( server, 0 ) == 0 );
+    snapshot_check( snapshot_server_num_connected_clients( server ) == 0 );
 
     // now reconnect the client and verify they connect
 
-    netcode_network_simulator_reset( network_simulator );
+    snapshot_check( snapshot_generate_connect_token( 1, &server_address, &server_address, TEST_CONNECT_TOKEN_EXPIRY, TEST_TIMEOUT_SECONDS, client_id, TEST_PROTOCOL_ID, private_key, user_data, connect_token ) == SNAPSHOT_OK );
 
-    check( netcode_generate_connect_token( 1, &server_address, &server_address, TEST_CONNECT_TOKEN_EXPIRY, TEST_TIMEOUT_SECONDS, client_id, TEST_PROTOCOL_ID, private_key, user_data, connect_token ) );
-
-    netcode_client_connect( client, connect_token );
+    snapshot_client_connect( client, connect_token );
 
     while ( 1 )
     {
-        netcode_network_simulator_update( network_simulator, time );
+        snapshot_client_update( client, time );
 
-        netcode_client_update( client, time );
+        snapshot_server_update( server, time );
 
-        netcode_server_update( server, time );
-
-        if ( netcode_client_state( client ) <= NETCODE_CLIENT_STATE_DISCONNECTED )
+        if ( snapshot_client_state( client ) <= SNAPSHOT_CLIENT_STATE_DISCONNECTED )
             break;
 
-        if ( netcode_client_state( client ) == NETCODE_CLIENT_STATE_CONNECTED )
+        if ( snapshot_client_state( client ) == SNAPSHOT_CLIENT_STATE_CONNECTED )
             break;
 
         time += delta_time;
     }
 
-    check( netcode_client_state( client ) == NETCODE_CLIENT_STATE_CONNECTED );
-    check( netcode_client_index( client ) == 0 );
-    check( netcode_server_client_connected( server, 0 ) == 1 );
-    check( netcode_server_num_connected_clients( server ) == 1 );
+    snapshot_check( snapshot_client_state( client ) == SNAPSHOT_CLIENT_STATE_CONNECTED );
+    snapshot_check( snapshot_client_index( client ) == 0 );
+    snapshot_check( snapshot_server_client_connected( server, 0 ) == 1 );
+    snapshot_check( snapshot_server_num_connected_clients( server ) == 1 );
 
-    netcode_server_destroy( server );
+    snapshot_server_destroy( server );
 
-    netcode_client_destroy( client );
-
-    netcode_network_simulator_destroy( network_simulator );
+    snapshot_client_destroy( client );
 }
-*/
 
 #define RUN_TEST( test_function )                                           \
     do                                                                      \
@@ -3058,8 +3038,10 @@ void test()
         RUN_TEST( test_client_error_connection_denied );
         RUN_TEST( test_client_side_disconnect );
         RUN_TEST( test_server_side_disconnect );
-        /*
+
         RUN_TEST( test_client_reconnect );
+
+        /*
         RUN_TEST( test_disable_timeout );
         */
     }
