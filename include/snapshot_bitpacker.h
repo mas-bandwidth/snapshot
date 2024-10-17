de05a50fdbe9e3a214bc81450ab50872a6e171e6
Glenn Fiedler <glenn@networknext.com>
2024-10-16 20:30:48 -0400

    more conversion work

--- include/snapshot_bitpacker.h
+++ include/snapshot_bitpacker.h
@@ -80,17 +80,6 @@ inline bool sequence_greater_than( uint16_t s1, uint16_t s2 )
            ( ( s1 < s2 ) && ( s2 - s1  > 32768 ) );
 }
 
-/**
-    Compares two 16 bit sequence numbers and returns true if the first one is less than the second (considering wrapping).
-    IMPORTANT: This is not the same as s1 < s2!
-    Greater than is defined specially to handle wrapping sequence numbers.
-    If the two sequence numbers are close together, it is as normal, but they are far apart, it is assumed that they have wrapped around.
-    Thus, sequence_less_than( 0, 1 ) returns true, and so does sequence_greater_than( 65535, 0 )!
-    @param s1 The first sequence number.
-    @param s2 The second sequence number.
-    @returns True if the s1 is less than s2, with sequence number wrapping considered.
- */
-
 inline bool sequence_less_than( uint16_t s1, uint16_t s2 )
 {
     return sequence_greater_than( s2, s1 );
@@ -100,23 +89,9 @@ inline bool sequence_less_than( uint16_t s1, uint16_t s2 )
 
 namespace snapshot
 {
-    /**
-        Bitpacks unsigned integer values to a buffer.
-        Integer bit values are written to a 64 bit scratch value from right to left.
-        Once the low 32 bits of the scratch is filled with bits it is flushed to memory as a dword and the scratch value is shifted right by 32.
-        The bit stream is written to memory in little endian order, which is considered network byte order for this library.
-     */
-
     class BitWriter
     {
     public:
-
-        /**
-            Bit writer constructor.
-            Creates a bit writer object to write to the specified buffer.
-            @param data The pointer to the buffer to fill with bitpacked data.
-            @param bytes The size of the buffer in bytes. Must be a multiple of 4, because the bitpacker reads and writes memory as dwords, not bytes.
-         */
 
         BitWriter( void * data, int bytes ) : m_data( (uint32_t*) data ), m_numWords( bytes / 4 )
         {
@@ -128,15 +103,6 @@ namespace snapshot
             m_scratch = 0;
             m_scratchBits = 0;
         }
-
-        /**
-            Write bits to the buffer.
-            Bits are written to the buffer as-is, without padding to nearest byte. Will assert if you try to write past the end of the buffer.
-            A boolean value writes just 1 bit to the buffer, a value in range [0,31] can be written with just 5 bits and so on.
-            IMPORTANT: When you have finished writing to your buffer, take care to call BitWrite::FlushBits, otherwise the last dword of data will not get flushed to memory!
-            @param value The integer value to write to the buffer. Must be in [0,(1<<bits)-1].
-            @param bits The number of bits to encode in [1,32].
-         */
 
         void WriteBits( uint32_t value, int bits )
         {
@@ -165,12 +131,6 @@ namespace snapshot
             m_bitsWritten += bits;
         }
 
-        /**
-            Write an alignment to the bit stream, padding zeros so the bit index becomes is a multiple of 8.
-            This is useful if you want to write some data to a packet that should be byte aligned. For example, an array of bytes, or a string.
-            IMPORTANT: If the current bit index is already a multiple of 8, nothing is written.
-         */
-
         void WriteAlign()
         {
             const int remainderBits = m_bitsWritten % 8;
@@ -182,14 +142,6 @@ namespace snapshot
                 snapshot_assert( ( m_bitsWritten % 8 ) == 0 );
             }
         }
-
-        /**
-            Write an array of bytes to the bit stream.
-            Use this when you have to copy a large block of data into your bitstream.
-            Faster than just writing each byte to the bit stream via BitWriter::WriteBits( value, 8 ), because it aligns to byte index and copies into the buffer without bitpacking.
-            @param data The byte array data to write to the bit stream.
-            @param bytes The number of bytes to write.
-         */
 
         void WriteBytes( const uint8_t * data, int bytes )
         {
@@ -232,11 +184,6 @@ namespace snapshot
             snapshot_assert( headBytes + numWords * 4 + tailBytes == bytes );
         }
 
-        /**
-            Flush any remaining bits to memory.
-            Call this once after you've finished writing bits to flush the last dword of scratch to memory!
-         */
-
         void FlushBits()
         {
             if ( m_scratchBits != 0 )
@@ -254,54 +201,25 @@ namespace snapshot
             }
         }
 
-        /**
-            How many align bits would be written, if we were to write an align right now?
-            @returns Result in [0,7], where 0 is zero bits required to align (already aligned) and 7 is worst case.
-         */
-
         int GetAlignBits() const
         {
             return ( 8 - ( m_bitsWritten % 8 ) ) % 8;
         }
-
-        /**
-            How many bits have we written so far?
-            @returns The number of bits written to the bit buffer.
-         */
 
         int GetBitsWritten() const
         {
             return m_bitsWritten;
         }
 
-        /**
-            How many bits are still available to write?
-            For example, if the buffer size is 4, we have 32 bits available to write, if we have already written 10 bytes then 22 are still available to write.
-            @returns The number of bits available to write.
-         */
-
         int GetBitsAvailable() const
         {
             return m_numBits - m_bitsWritten;
         }
 
-        /**
-            Get a pointer to the data written by the bit writer.
-            Corresponds to the data block passed in to the constructor.
-            @returns Pointer to the data written by the bit writer.
-         */
-
         const uint8_t * GetData() const
         {
             return (uint8_t*) m_data;
         }
-
-        /**
-            The number of bytes flushed to memory.
-            This is effectively the size of the packet that you should send after you have finished bitpacking values with this class.
-            The returned value is not always a multiple of 4, even though we flush dwords to memory. You won't miss any data in this case because the order of bits written is designed to work with the little endian memory layout.
-            IMPORTANT: Make sure you call BitWriter::FlushBits before calling this method, otherwise you risk missing the last dword of data.
-         */
 
         int GetBytesWritten() const
         {
@@ -310,32 +228,18 @@ namespace snapshot
 
     private:
 
-        uint32_t * m_data;              ///< The buffer we are writing to, as a uint32_t * because we're writing dwords at a time.
-        uint64_t m_scratch;             ///< The scratch value where we write bits to (right to left). 64 bit for overflow. Once # of bits in scratch is >= 32, the low 32 bits are flushed to memory.
-        int m_numBits;                  ///< The number of bits in the buffer. This is equivalent to the size of the buffer in bytes multiplied by 8. Note that the buffer size must always be a multiple of 4.
-        int m_numWords;                 ///< The number of words in the buffer. This is equivalent to the size of the buffer in bytes divided by 4. Note that the buffer size must always be a multiple of 4.
-        int m_bitsWritten;              ///< The number of bits written so far.
-        int m_wordIndex;                ///< The current word index. The next word flushed to memory will be at this index in m_data.
-        int m_scratchBits;              ///< The number of bits in scratch. When this is >= 32, the low 32 bits of scratch is flushed to memory as a dword and scratch is shifted right by 32.
+        uint32_t * m_data;
+        uint64_t m_scratch;
+        int m_numBits;
+        int m_numWords;
+        int m_bitsWritten;
+        int m_wordIndex;
+        int m_scratchBits;
     };
-
-    /**
-        Reads bit packed integer values from a buffer.
-        Relies on the user reconstructing the exact same set of bit reads as bit writes when the buffer was written. This is an unattributed bitpacked binary stream!
-        Implementation: 32 bit dwords are read in from memory to the high bits of a scratch value as required. The user reads off bit values from the scratch value from the right, after which the scratch value is shifted by the same number of bits.
-     */
 
     class BitReader
     {
     public:
-
-        /**
-            Bit reader constructor.
-            Non-multiples of four buffer sizes are supported, as this naturally tends to occur when packets are read from the network.
-            However, actual buffer allocated for the packet data must round up at least to the next 4 bytes in memory, because the bit reader reads dwords from memory not bytes.
-            @param data Pointer to the bitpacked data to read.
-            @param bytes The number of bytes of bitpacked data to read.
-         */
 
 #if SNAPSHOT_ASSERTS
         BitReader( const void * data, int bytes ) : m_data( (const uint32_t*) data ), m_numBytes( bytes ), m_numWords( ( bytes + 3 ) / 4)
@@ -351,24 +255,10 @@ namespace snapshot
             m_wordIndex = 0;
         }
 
-        /**
-            Would the bit reader would read past the end of the buffer if it read this many bits?
-            @param bits The number of bits that would be read.
-            @returns True if reading the number of bits would read past the end of the buffer.
-         */
-
         bool WouldReadPastEnd( int bits ) const
         {
             return m_bitsRead + bits > m_numBits;
         }
-
-        /**
-            Read bits from the bit buffer.
-            This function will assert in debug builds if this read would read past the end of the buffer.
-            In production situations, the higher level ReadStream takes care of checking all packet data and never calling this function if it would read past the end of the buffer.
-            @param bits The number of bits to read in [1,32].
-            @returns The integer value read in range [0,(1<<bits)-1].
-         */
 
         uint32_t ReadBits( int bits )
         {
@@ -402,14 +292,6 @@ namespace snapshot
             return output;
         }
 
-        /**
-            Read an align.
-            Call this on read to correspond to a WriteAlign call when the bitpacked buffer was written.
-            This makes sure we skip ahead to the next aligned byte index. As a safety check, we verify that the padding to next byte is zero bits and return false if that's not the case.
-            This will typically abort packet read. Just another safety measure...
-            @returns True if we successfully read an align and skipped ahead past zero pad, false otherwise (probably means, no align was written to the stream).
-         */
-
         bool ReadAlign()
         {
             const int remainderBits = m_bitsRead % 8;
@@ -422,10 +304,6 @@ namespace snapshot
             }
             return true;
         }
-
-        /**
-            Read bytes from the bitpacked data.
-         */
 
         void ReadBytes( uint8_t * data, int bytes )
         {
@@ -466,31 +344,15 @@ namespace snapshot
             snapshot_assert( headBytes + numWords * 4 + tailBytes == bytes );
         }
 
-        /**
-            How many align bits would be read, if we were to read an align right now?
-            @returns Result in [0,7], where 0 is zero bits required to align (already aligned) and 7 is worst case.
-         */
-
         int GetAlignBits() const
         {
             return ( 8 - m_bitsRead % 8 ) % 8;
         }
 
-        /**
-            How many bits have we read so far?
-            @returns The number of bits read from the bit buffer so far.
-         */
-
         int GetBitsRead() const
         {
             return m_bitsRead;
         }
-
-        /**
-            How many bits are still available to read?
-            For example, if the buffer size is 4, we have 32 bits available to read, if we have already written 10 bytes then 22 are still available.
-            @returns The number of bits available to read.
-         */
 
         int GetBitsRemaining() const
         {
@@ -499,16 +361,16 @@ namespace snapshot
 
     private:
 
-        const uint32_t * m_data;            ///< The bitpacked data we're reading as a dword array.
-        uint64_t m_scratch;                 ///< The scratch value. New data is read in 32 bits at a top to the left of this buffer, and data is read off to the right.
-        int m_numBits;                      ///< Number of bits to read in the buffer. Of course, we can't *really* know this so it's actually m_numBytes * 8.
-        int m_numBytes;                     ///< Number of bytes to read in the buffer. We know this, and this is the non-rounded up version.
+        const uint32_t * m_data;
+        uint64_t m_scratch;
+        int m_numBits;
+        int m_numBytes;
 #if SNAPSHOT_ASSERTS
-        int m_numWords;                     ///< Number of words to read in the buffer. This is rounded up to the next word if necessary.
+        int m_numWords;
 #endif // #if SNAPSHOT_ASSERTS
-        int m_bitsRead;                     ///< Number of bits read from the buffer so far.
-        int m_scratchBits;                  ///< Number of bits currently in the scratch value. If the user wants to read more bits than this, we have to go fetch another dword from memory.
-        int m_wordIndex;                    ///< Index of the next word to read from memory.
+        int m_bitsRead;
+        int m_scratchBits;
+        int m_wordIndex;
     };
 }
 
